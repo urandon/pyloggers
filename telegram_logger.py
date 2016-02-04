fba12fb941e721460818fb29f880d3d4446abbbe
urandon <inikitah@gmail.com>
2016-02-05 02:13:01 +0300

    Telegram flush + cosmetics

--- telegram_logger.py
+++ telegram_logger.py
@@ -10,6 +10,8 @@ class NullLogger(object):
         pass
 
     def push(self, string, flush=True):
+        if flush:
+            self.flush()
         return self
 
     def flush(self):
@@ -20,17 +22,19 @@ class NullLogger(object):
 class TelegramLogger(NullLogger):
     def __init__(self,
                  token="72500331:AAGPGC4dI8Co3NJWHxno_HBGhLf57F1xHWA",
-                 name="logger@:",
-                 reader_id=None,
-                 print_dual_logging=True):
-        self.updater = Updater(token=token)
-        self.dispatcher = self.updater.dispatcher
-        self.bot = None
+                 name="logger",
+                 reader_chat_id=None,
+                 print_dual_logging=True,
+                 max_local_log_size=5):
         self.name = name
         self.log_queue = []
-        self.reader_id = reader_id
         self.reader_chat_id = None
         self.print_dual_logging = print_dual_logging
+        self.max_local_log_size = max_local_log_size
+
+        self.bot = None
+        self.updater = Updater(token=token)
+        self.dispatcher = self.updater.dispatcher
         self.dispatcher.addTelegramCommandHandler('reader', self.set_reader)
         self.dispatcher.addTelegramCommandHandler('ping', self.pong)
         self.dispatcher.addTelegramCommandHandler('help', self.help)
@@ -38,14 +42,22 @@ class TelegramLogger(NullLogger):
         self.launch()
 
     def pong(self, bot, update):
-        bot.sendMessage(chat_id=update.message.chat_id, text='pong')
+        bot.sendMessage(
+            chat_id=update.message.chat_id,
+            text='<{}> pong'.format(self.name))
 
     def help(self, bot, update):
         msg = '/ping -- echo, check bot if it is alive\n'\
-              '/reader -- set me as log reader'
+              '/reader [name] -- set me as log reader'
         bot.sendMessage(chat_id=update.message.chat_id, text=msg)
 
     def set_reader(self, bot, update):
+        text = update.message.text.strip()
+        if len(text.split()) > 1:
+            name = ' '.join(text.split()[1:]).strip().split()[0]
+            if name != self.name:
+                return
+
         self.reader_chat_id = update.message.chat_id
         self.bot = bot
         self.send_to_reader('You are a reader now')
@@ -53,21 +65,23 @@ class TelegramLogger(NullLogger):
 
     def send_to_reader(self, msg):
         self.bot.sendMessage(chat_id=self.reader_chat_id,
-                             text=self.name + " " + msg)
+                             text='<{}> {}'.format(self.name, msg))
 
     def push(self, string, flush=True):
         self.log_queue.append(string)
         if self.print_dual_logging:
             print string
-        self.on_update()
+        if flush or len(self.log_queue) > self.max_local_log_size:
+            self.flush()
         return self
 
-    def on_update(self):
+    def flush(self):
         if self.reader_chat_id is None or self.bot is None:
             return
         for msg in self.log_queue:
             self.send_to_reader(msg)
         self.log_queue = []
+        return self
 
     def launch(self):
         self.updater.start_polling()
